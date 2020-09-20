4a38c5c4266b0fe9d0756b87497b88c907618cd3
frankQDU <qdufrankliu@163.com>
2020-09-20 19:16:17 +0800

    update

--- feature_eng.py
+++ feature_eng.py
@@ -1,3 +1,46 @@
+import category_encoders as ce
+
+def cal_woe(df_tr, col):
+    enc =  ce.WOEEncoder(cols=[col]).fit(df_tr.loc[::,feature_col],
+                                                    df_tr.loc[::,'isDefault'])
+    tmp = pd.DataFrame({f'{col}':df_tr.loc[::,col],
+                        f'woe_{col}':enc.transform(df_tr.loc[::,feature_col],
+                                                   df_tr.loc[::,'isDefault'])[col]})
+    return tmp.groupby([col])[f'woe_{col}'].mean()
+
+def cal_cat(df_tr, col):
+    enc =  ce.cat_boost.CatBoostEncoder(cols=[col]).fit(df_tr.loc[::,feature_col],
+                                                        df_tr.loc[::,'isDefault'])
+    tmp = pd.DataFrame({f'{col}':df_tr.loc[::,col],
+                        f'cat_{col}':enc.transform(df_tr.loc[::,feature_col],
+                                                   df_tr.loc[::,'isDefault'])[col]})
+    return tmp.groupby([col])[f'cat_{col}'].mean()
+
+def cal_js(df_tr, col):
+    enc =  ce.james_stein.JamesSteinEncoder(cols=[col]).fit(df_tr.loc[::,feature_col],
+                                                        df_tr.loc[::,'isDefault'])
+    tmp = pd.DataFrame({f'{col}':df_tr.loc[::,col],
+                        f'js_{col}':enc.transform(df_tr.loc[::,feature_col],
+                                                   df_tr.loc[::,'isDefault'])[col]})
+    return tmp.groupby([col])[f'js_{col}'].mean()
+
+def cal_loe(df_tr, col):
+    enc =  ce.LeaveOneOutEncoder(cols=[col]).fit(df_tr.loc[::,feature_col],
+                                                        df_tr.loc[::,'isDefault'])
+    tmp = pd.DataFrame({f'{col}':df_tr.loc[::,col],
+                        f'loe_{col}':enc.transform(df_tr.loc[::,feature_col],
+                                                   df_tr.loc[::,'isDefault'])[col]})
+    return tmp.groupby([col])[f'loe_{col}'].mean()
+
+def cal_moe(df_tr, col):
+    enc =  ce.MEstimateEncoder(cols=[col]).fit(df_tr.loc[::,feature_col],
+                                                        df_tr.loc[::,'isDefault'])
+    tmp = pd.DataFrame({f'{col}':df_tr.loc[::,col],
+                        f'moe_{col}':enc.transform(df_tr.loc[::,feature_col],
+                                                   df_tr.loc[::,'isDefault'])[col]})
+    return tmp.groupby([col])[f'moe_{col}'].mean()
+
+
 def kfold_stats_feature(df_tr, df_te, feats, n_splits):
     folds = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=2020)  # 这里最好和后面模型的K折交叉验证保持一致
 
